a456669a8ed8c84d40de5fd69f7603f791279287
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-08-15 17:36:09 -0300

    Create account component

--- src/telas/Cadastro/index.js
+++ src/telas/Cadastro/index.js
@@ -1,25 +1,21 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
 import Botao from "../../componentes/Botao";
 import { EntradaTexto } from "../../componentes/EntradaTexto";
 import estilos from "./estilos";
-import { auth } from "../../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth/";
+import { cadastrar } from "../../servicos/requisicoesFirebase";
 
 export default function Cadastro({ navigation }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
   const [confirmaSenha, setConfirmaSenha] = useState("");
 
-  useEffect(() => {
-    createUserWithEmailAndPassword(auth, "teste2@email.com", "123456")
-      .then((dadosDoUsuarios) => {
-        console.log(dadosDoUsuarios);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+  async function realizarCadastro() {
+    await cadastrar(email, senha, confirmaSenha);
+    setEmail('');
+    setSenha('');
+    setConfirmaSenha('');
+  }
 
   return (
     <View style={estilos.container}>
@@ -42,7 +38,7 @@ export default function Cadastro({ navigation }) {
         secureTextEntry
       />
 
-      <Botao onPress={() => {}}>CADASTRAR</Botao>
+      <Botao onPress={() => realizarCadastro()}>CADASTRAR</Botao>
     </View>
   );
 }
