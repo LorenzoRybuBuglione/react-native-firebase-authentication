d44a252cf95544e011ab66a986457ad1746413c1
Lorenzo Rybu Buglione <lorenzorybuglione@gmail.com>
2023-08-15 17:56:46 -0300

    Create Account Errors treated

--- src/servicos/requisicoesFirebase.js
+++ src/servicos/requisicoesFirebase.js
@@ -1,12 +1,33 @@
 import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, AuthErrorCodes } from "firebase/auth";
 
-export async function cadastrar(email, senha, confirmaSenha) {
-  createUserWithEmailAndPassword(auth, email, senha)
-  .then((dadosDoUsuario) => {
-    console.log(dadosDoUsuario)
-  })
-  .catch((error) => {
-    console.log(error)
-  });
+function errosFirebase(error) {
+  let mensagem = "";
+  switch (error.code) {
+    case AuthErrorCodes.EMAIL_EXISTS:
+      mensagem = "Esse e-mail já está cadastrado";
+      break;
+    case AuthErrorCodes.INVALID_EMAIL:
+      mensagem = "E-mail inválido";
+      break;
+    case AuthErrorCodes.WEAK_PASSWORD:
+      mensagem = "A senha precisa ter no mínimo 6 caracteres";
+      break;
+    default:
+      mensagem = "Erro desconhecido";
+  }
+  return mensagem;
+}
+
+export async function cadastrar(email, senha) {
+  const resultado = await createUserWithEmailAndPassword(auth, email, senha)
+    .then((dadosDoUsuario) => {
+      console.log(dadosDoUsuario);
+      return "sucesso";
+    })
+    .catch((error) => {
+      console.log(error);
+      return errosFirebase(error);
+    });
+    return resultado;
 }
